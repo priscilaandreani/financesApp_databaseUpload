0fb7e61b2e7e130b64bf2fd7b359b93b62b36ef4
Priscila Ravanelli <priscila.andreani@gmail.com>
2021-03-07 09:47:21 -0300

    new solution to challange

--- src/database/migrations/1598716617797-CreateTransactions.ts
+++ src/database/migrations/1598716617797-CreateTransactions.ts
@@ -9,7 +9,7 @@ export default class CreateTransactions1598716617797
         columns: [
           {
             name: 'id',
-            type: 'uuid',
+            type: 'uuid ',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -19,16 +19,14 @@ export default class CreateTransactions1598716617797
             type: 'varchar',
           },
           {
-            name: 'value',
-            type: 'decimal',
-          },
-          {
             name: 'type',
             type: 'varchar',
           },
           {
-            name: 'category',
-            type: 'varchar',
+            name: 'value',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
           },
           {
             name: 'created_at',

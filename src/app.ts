d1f41bcadde8243f6e15a4220ccae2753898b736
Priscila Ravanelli <priscila.andreani@gmail.com>
2021-03-07 21:23:27 -0300

    adaptação ao front-end

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
@@ -6,12 +7,10 @@ import createConnection from './database';
 import AppError from './errors/AppError';
 import routes from './routes';
 
-
-
-
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
